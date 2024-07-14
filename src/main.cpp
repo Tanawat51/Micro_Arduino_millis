0f6ac326246b221d667f55ab6187bd85679cf25a
Aejaeshi <Aejaeshi2013@gmail.com>
2024-07-14 16:41:59 +0700

    Debounce Program

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,34 @@
 #include <Arduino.h>
+#define BTN 12
+int counterVal = 0;
+boolean buttonState;
+
+
 
 void setup() {
-Serial.begin(9600);
-Serial.println("Start Program");
+  Serial.begin(9600);
+  pinMode(BTN, OUTPUT);
 }
 
 void loop() {
-  unsigned long time1 = millis();
-  Serial.println(time1);
-  delay(1000);
+  if (debounceButton(BTN) == HIGH && buttonState == LOW) {
+    counterVal = counterVal + 1;
+    Serial.print("Counter : ");
+    Serial.println(counterVal);
+    buttonState = HIGH;
+  }
+  else if (debounceButton(BTN) == LOW && buttonState == HIGH)
+  {
+    buttonState = LOW;
+  }
 }
-
+// ฟังก์ชั่น Debounce on a Push Button
+boolean debounceButton(boolean state) {
+  boolean stateNow = digitalRead(BTN);
+  if (state != stateNow)
+  {
+    delay(10);
+    stateNow = digitalRead(BTN);
+  }
+  return stateNow;
+}
